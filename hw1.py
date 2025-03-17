9b7034a8bcea997c71dde72e6f340b2de7bb609d
park-siyeon-tech <stump4711@gmail.com>
2025-03-17 23:12:22 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -5,13 +5,11 @@ def get_radius(prompt):
 
 def get_circle_area(radius):
     s=radius*radius*3.14
-    answer='반지름이 '+ str(radius)+'인 원의 넓이 = '+str(radius)+" X "+str(radius)+" X 3.14 = "+str(s)
-    return  answer
+    return s
 
-#==================================================================
-print('시작합니다')
+print('시작')
 input_radius ="넓이를 구하고자 하는 원의 반지름은?"
-Rresult= get_radius(input_radius)
-Sresult=get_circle_area(Rresult)
-print(Sresult)
-print('종료합니다')
+r_result= get_radius(input_radius)
+s_result=get_circle_area(r_result)
+print('반지름이',r_result,'인 원의넓이는=3.14 X',r_result,'X',r_result,'=',s_result)
+print('끝')
