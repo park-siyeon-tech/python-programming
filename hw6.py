f296ca0822b8ff7de027452dae559bcf06f276fa
park-siyeon-tech <stump4711@gmail.com>
2025-04-17 14:42:56 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,17 @@
+for n in range(1,10):
+    i=2
+    while i<=5:
+        print(f'{i} x {n} = {n*i:2d}',end='\t')
+        i+=1
+    print()
+    
+print()
+
+for n in range(1,10):
+    i=6
+    while i<=9:
+        print(f'{i} x {n} = {n*i:2d}',end='\t')
+        i+=1
+    print()
+    
+        
